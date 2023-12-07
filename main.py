2eb78fff5d7795a30ed856cff23ecd6c55571d4f
wentung <wentungwen@gmail.com>
2023-12-07 21:23:46 +0100

    feat: init

---
+++ main.py
@@ -0,0 +1,92 @@
+import json
+import datetime
+import os
+
+from twilio.rest import Client
+from selenium import webdriver
+
+# Twilio account details
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+twilio_phone_number = os.environ.get('TWILIO_PHONE_NUMBER')
+receiver_phone_number = os.environ.get('RECEIVER_PHONE_NUMBER')
+url = os.environ.get('URL')
+previous_listings_file = os.environ.get('PREVIOUS_LISTINGS_FILE')
+
+print(account_sid)
+# Function to send the SMS notification
+def send_sms(msg):
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_=twilio_phone_number,
+        body=msg,
+        to=receiver_phone_number
+    )
+    print('SMS notification sent!')
+
+
+# Function to load previously scraped listings from file
+def load_previous_listings():
+    try:
+        with open(previous_listings_file, 'r') as file:
+            previous_house_list = json.load(file)
+    except FileNotFoundError:
+        previous_house_list = []
+        with open(previous_listings_file, 'w') as file:
+            json.dump(previous_house_list, file)
+    return previous_house_list
+
+
+# Function to scrape new houses
+def search_for_new_houses():
+    driver = webdriver.Chrome()
+    driver.get(url)
+    scraped_new_houses = []
+    housing_titles = driver.find_elements("xpath", "//span[contains(@class, 'address-part')]")
+    house = {}
+    current_idx = 0
+    try:
+        for idx, n in enumerate(housing_titles):
+            if idx % 2 == 0:
+                house["title"] = n.text
+            else:
+                house["location"] = n.text
+                scraped_new_houses.append(house)
+                current_idx += 1
+        driver.quit()
+        return scraped_new_houses
+    except Exception as e:
+        print(e)
+
+
+def new_added_houses(new, pre):
+    added_houses = []
+    for house in new:
+        if house not in pre:
+            added_houses.append(house)
+    return added_houses
+
+
+def generate_msg_text(new_added):
+    txt = f"New houses on {url}\n"
+    for idx, house in enumerate(new_added):
+        txt += f"{idx}. {house['title']}: {house['location']}\n"
+    return txt
+
+
+# Function to save updated listings to file
+def update_listings(listings):
+    with open(previous_listings_file, 'a') as file:
+        json.dump(new_added_houses, file)
+
+
+previous_houses = load_previous_listings()
+# new_houses = search_for_new_houses()
+new_houses = [{'title': 'testest8', 'location': 'testt'}, {'title': 'testedwst8', 'location': 'testt'}, {'title': 'Professor de Moorplein 278', 'location': 'Tilburg | West | De Reit'}, {'title': 'Professor de Moorplein 278', 'location': 'Tilburg | West | De Reit'}, {'title': 'Professor de Moorplein 278', 'location': 'Tilburg | West | De Reit'}, {'title': 'Professor de Moorplein 278', 'location': 'Tilburg | West | De Reit'}, {'title': 'Professor de Moorplein 278', 'location': 'Tilburg | West | De Reit'}, {'title': 'Professor de Moorplein 278', 'location': 'Tilburg | West | De Reit'}, {'title': 'Professor de Moorplein 278', 'location': 'Tilburg | West | De Reit'}, {'title': 'Professor de Moorplein 278', 'location': 'Tilburg | West | De Reit'}]
+new_added_houses = new_added_houses(new_houses, previous_houses)
+msg = generate_msg_text(new_added_houses)
+
+print(msg)
+send_sms(msg)
+# update_listings()
+
