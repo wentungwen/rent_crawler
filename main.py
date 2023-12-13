9076921052ca0ad15bc22c5ad8df1718c3acb418
wentung <wentungwen@gmail.com>
2023-12-13 18:09:06 +0100

    refactor and connect to postgresql

--- main.py
+++ main.py
@@ -1,50 +1,68 @@
 from twilio.rest import Client
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import psycopg2
 import time
 import json
 import os
 
 
 # Twilio account details
-account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
-twilio_phone_number = os.environ.get('TWILIO_PHONE_NUMBER')
-receiver_phone_number = os.environ.get('RECEIVER_PHONE_NUMBER')
-url = os.environ.get('URL')
-previous_listings_file = os.environ.get('PREVIOUS_LISTINGS_FILE')
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+twilio_phone_number = os.environ.get("TWILIO_PHONE_NUMBER")
+receiver_phone_number = os.environ.get("RECEIVER_PHONE_NUMBER")
+url = os.environ.get("URL")
+previous_listings_file = os.environ.get("PREVIOUS_LISTINGS_FILE")
+db_password = os.environ.get("DB_PASSWORD")
+db_user = os.environ.get("DB_USER")
+
+
+# connect to database
+conn = psycopg2.connect(
+    host="localhost", database="rent", user=db_user, password=db_password, port="5432"
+)
+cur = conn.cursor()
+
+
+def update_db(houses):
+    houses_tuple = [(house["title"], house["location"]) for house in houses]
+
+    # delete and recreate table
+    cur.execute("DROP TABLE IF EXISTS houses")
+    cur.execute(
+        """ CREATE TABLE IF NOT EXISTS houses(
+                id SERIAL PRIMARY KEY,
+                title TEXT,
+                location TEXT
+    )"""
+    )
+
+    insert_script = "INSERT INTO houses(title, location) VALUES (%s, %s)"
+    cur.executemany(insert_script, houses_tuple)
+    conn.commit()
 
 
 # Function to send the SMS notification
 def send_sms(msg):
     client = Client(account_sid, auth_token)
     message = client.messages.create(
-        from_=f"{twilio_phone_number}",
-        body=msg,
-        to=f"{receiver_phone_number}"
+        from_=f"{twilio_phone_number}", body=msg, to=f"{receiver_phone_number}"
     )
-    print('SMS notification sent!')
+    print("SMS notification sent!")
 
 
-# Function to load previously scraped listings from file
-def load_previous_listings():
-    try:
-        with open(previous_listings_file, 'r') as file:
-            previous_house_list = json.load(file)
-    except FileNotFoundError:
-        previous_house_list = []
-        with open(previous_listings_file, 'w') as file:
-            json.dump(previous_house_list, file)
-    return previous_house_list
+# Function to load previously housing from database
+def load_previous_house():
+    cur.execute("SELECT * FROM houses")
+    previous_house = cur.fetchall()
+    return previous_house
+
 
 
 # Function to scrape new houses
 def search_for_new_houses():
-
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
@@ -55,10 +73,11 @@ def search_for_new_houses():
 
     driver.get(url)
     time.sleep(15)
-    # wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
 
     scraped_new_houses = []
-    housing_titles = driver.find_elements("xpath", "//span[contains(@class, 'address-part')]")
+    housing_titles = driver.find_elements(
+        "xpath", "//span[contains(@class, 'address-part')]"
+    )
     current_idx = 0
     try:
         for idx, n in enumerate(housing_titles):
@@ -82,21 +101,17 @@ def generate_msg_text(new_added):
     return txt
 
 
-# Function to save updated listings to file
-def update_listings(new_added):
-    with open(previous_listings_file, 'r') as file:
-        previous_houses = json.load(file)
-    previous_houses.extend(new_added)
-    with open(previous_listings_file, 'w') as file:
-        json.dump(previous_houses, file, indent=4)
-
-
 def new_added_houses(new, pre):
+    print("new", new)
+    print("pre", pre)
     added_houses = []
     for house in new:
         house_exists = False
         for prev_house in pre:
-            if house["title"] == prev_house["title"] and house["location"] == prev_house["location"]:
+            if (
+                house["title"] == prev_house[1]
+                and house["location"] == prev_house[2]
+            ):
                 house_exists = True
                 break
         if not house_exists:
@@ -105,18 +120,21 @@ def new_added_houses(new, pre):
 
 
 def main():
-    previous_houses = load_previous_listings()
+    previous_houses = load_previous_house()
     new_houses = search_for_new_houses()
     new_added = new_added_houses(new_houses, previous_houses)
-    print(previous_houses, new_houses, new_added)
+    print("previous_houses", previous_houses)
+    print("new_houses", new_houses)
+    print("new_added", new_added)
     if new_added:
         msg = generate_msg_text(new_added)
-        # send_sms(msg)
-        update_listings(new_added)
-        print(f'{ len(new_added) } houses found!')
+        send_sms(msg)
+        update_db(new_houses)
+        print(f"{ len(new_added) } houses found!")
     else:
-        print('No new houses found!')
+        print("No new houses found!")
 
 
 main()
-print('Done!')
+cur.close()
+conn.close()
